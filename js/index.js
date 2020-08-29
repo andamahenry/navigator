20c2a49b40a330298a486526cd6d00792583ee54
andamahenry <33576566+andamahenry@users.noreply.github.com>
2020-08-29 23:46:34 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
 "use strict"
-var map;
+var map;let theMarker = 'null',myLocation;
+
 window.onload = function(){
 	initializer();
 	map.on('click',onmapclick);
 }
 function initializer(){
 	// initialize Leaflet
-	map = L.map('map').setView({lon: 32.54700064661847, lat: 0.3350024208544684}, 2);//: 0.3350024208544684,32.54700064661847
+	map = L.map('map').setView({lon: 32.54700064661847, lat: 0.3350024208544684},12);//: 0.3350024208544684,32.54700064661847
 
 	// add the OpenStreetMap tiles
 	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -17,15 +18,25 @@ function initializer(){
 	// show the scale bar on the lower left corner
 	L.control.scale().addTo(map);
 
-	// show a marker on the map
 	//L.marker({lon: 0, lat: 0}).bindPopup('The center of the world').addTo(map);
+	
 }
-function onmapclick(e){
-	//console.log(e);
-	var clicked = e.latlng;
-	L.marker();
-	console.log(e.latlng);
-	console.log(e.latlng.lat,e.latlng.lng);
+function onmapclick(){
+	if(window.navigator){
+		window.navigator.geolocation.getCurrentPosition((position)=>{
+			console.log(position.coords.latitude,position.coords.longitude);
+		
+			// show a marker on the map
+			if(theMarker == 'null' || theMarker == 'undefined'){//check if there exists any marker
+				theMarker = L.marker({lon: position.coords.longitude,lat: position.coords.latitude}).bindPopup("You're Likely To Be Here!").addTo(map);
+			}else{//remove any existing markers
+				map.removeLayer(theMarker);
+			}
+				theMarker = 'undefined';
+		});
+	}else{
+		window.alert("Geolocation is not supported by the device platform");
+	}
 	
 	//L.marker();
 }
